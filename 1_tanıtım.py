d9e0e09f14fa555bb7033642eaf5988d050e2e8b
ozkanyazici1992 <58921061+ozkanyazici1992@users.noreply.github.com>
2023-12-15 20:18:38 +0300

    Add files via upload

---
+++ 1_tanıtım.py
@@ -0,0 +1,45 @@
+import pygame, random
+
+pygame.init()
+
+genislik = 1400
+yukseklik = 800
+
+goruntu_yuzeyi = pygame.display.set_mode((genislik, yukseklik))
+
+FPS = 30
+saat = pygame.time.Clock()
+
+class Oyun():
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def cizmek(self):
+        pass
+    def carpisma_tespiti(self):
+        pass
+    def yeni_round_baslama(self):
+       pass
+    def yeni_hedef_secenegi(self):
+        pass
+    def oyun_durdu(self, ana_metin, baslik_metni):
+        pass
+    def oyun_yenileme(self):
+        pass
+
+class Oyuncu(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def joker(self):
+        pass
+    def yenileme(self):
+        pass
+
+class Canavar(pygame.sprite.Sprite):
+    def __init__(self:)
+        pass
+    def update(self):
+        pass
